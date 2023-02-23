a09f15e7e26059da4e895560441bfcf78a7e6ca7
arjavpoudel <123589124+arjavpoudel@users.noreply.github.com>
2023-02-23 16:00:40 +0000

    big update
    input message string instead of int for more practical use. 
    User has to input correct d value for the message to decrypted correctly. otherwise, failure message.
    input string converted to list of chars. ordinal values of char summed over to get number input.
    number input converted to hex for encryption stage.

--- main.py
+++ main.py
@@ -1,64 +1,49 @@
 import random
-random.seed(14)
-
-#intializes 2 random prime numbers, stores their product as N and their totient as phi_N
-p = 23
-q = 31
-N = p*q
-phi_N = (p-1) * (q-1)
+from helper_functions import get_prime_candidates, get_private_key, is_prime, get_coprime, wordToNum
 
 
-#returns Greatest Common Divisor between p and q
-def GCD(p,q):
-    while q!= 0:
-        p, q = q, p%q
-    return p
+def main():
+    # Generate two random prime numbers
+    p_candidates = get_prime_candidates(2, pow(10,4))
+    p = random.choice(p_candidates)
+    q_candidates = [q for q in p_candidates if q != p]
+    q = random.choice(q_candidates)
+    #print(f"p is: {p}")
+    #print(f"q is: {q}")
 
-'''
-takes two integers p and q as input, and returns p if it is coprime to q (i.e., if their GCD is 1). 
-If p is equal to 1 or not coprime to q, then the function returns nothing
-'''
-def get_coprime(p,q):
-    if p ==1:
-        pass
-    elif GCD(p,q) == 1:
-        return p
+    # Compute N and phi_N
+    N = p * q
+    phi_N = (p - 1) * (q - 1)
+
+    # Generate the public key
+    E = get_coprime(phi_N, N)
+    #print(f"E is: {E}")
+    public_key = (E, N)
+    print(f"public key: {public_key}")
+
+    # Generate the private key
+    D = get_private_key(E, phi_N)
+    #print(f"D is: {D}")
+    private_key = (D, N)
+    print(f"private key: {private_key}")
+
+    # Encrypt and decrypt a message
+    raw_message = input("Enter your message here: ")
+    integer_message = wordToNum(raw_message)
+    print(f"integer message: {integer_message}")
+    encrypted = hex(pow(integer_message, E, N))
+    print(f"encrypted message: {encrypted}")
+
+    d_test = int(input("enter first index val of private key to decrypt: "))
+
+    decrypted = pow(int(encrypted,16), d_test, N)
+    print(f"decrypted integer message: {decrypted}")
+    if decrypted == integer_message:
+        print("success!")
+        print(f"original message: {raw_message}")
+        
     else:
-        pass
+        print("decryption unsuccessful.")
 
-    
-'''
-For each number e, the get_coprime function is called with parameters e and N, 
-and the result is appended to the public_key_0 list.
-'''
-
-coprime_list = [e for e in range(1, phi_N) if GCD(e, N) == 1 and GCD(e, phi_N) == 1]
-public_key_candidates = [e for e in coprime_list if get_coprime(e, phi_N) in coprime_list]
-
-E = random.choice(public_key_candidates)
-print(f"E is: {E}")
-
-private_key_candidates = [D for D in range(1,N) if (E*D) % phi_N ==1]
-
-D = random.choice(private_key_candidates)
-print(f"D is: {D}")
-
-print(f"the public key is {E,N}")
-
-
-while True:
-    message = int(input(f"Pick a number less than {N}: "))
-    if message >= N:
-        print()
-        print(f"Warning: Number outwith range. Please try again.")
-    else:
-        break
-
-encrypt  = (message**E)%N
-
-print(f"The encrypted message is: {encrypt}")
-
-decrypt= (encrypt**D)%N
-print(f"Your original message was: {decrypt}")
-
-
+if __name__ == '__main__':
+    main()
