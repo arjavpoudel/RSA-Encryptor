2ed2df0db186fc596436be74a3b7ba5fc9871806
arjavpoudel <123589124+arjavpoudel@users.noreply.github.com>
2023-02-23 12:06:05 +0000

    for loops -> list comprehensions
    replaced extraneous for loops with list comprehensions for brevity. Instead of generating 2 seperate public_key_lists and another (e candidates[]) with the common elements from public_key_1 and public_key_2, combined logic into 1 distinct list comp. reduced 26 lines of code to just 8 (69% improvement).

--- main.py
+++ main.py
@@ -26,49 +26,24 @@ def get_coprime(p,q):
     else:
         pass
 
-
+    
 '''
 For each number e, the get_coprime function is called with parameters e and N, 
 and the result is appended to the public_key_0 list.
 '''
-coprime_list_0 = range(1,phi_N)
-public_key_0 = []
-for e in coprime_list_0:
-        public_key_0.append(get_coprime(e,N))
 
-coprime_list_1 = range(1,phi_N)
-public_key_1 = []
-for e in coprime_list_1:
-        public_key_1.append(get_coprime(e,phi_N))
+coprime_list = [e for e in range(1, phi_N) if GCD(e, N) == 1 and GCD(e, phi_N) == 1]
+public_key_candidates = [e for e in coprime_list if get_coprime(e, phi_N) in coprime_list]
 
-
-
-public_key_2 =[]
-for elem in public_key_1:
-    if elem in public_key_0:
-        public_key_2.append(elem)
-
-
-e_candidates =[]
-for elem in public_key_2:
-    if elem != None:
-        e_candidates.append(elem)
-
-E = random.choice(e_candidates)
+E = random.choice(public_key_candidates)
 print(f"E is: {E}")
 
-private_key_0 = range(1,N)
-private_key_1 = []
-for D in private_key_0:
-    if E*D % phi_N == 1:
-       private_key_1.append(D)
-D = random.choice(private_key_1)
+private_key_candidates = [D for D in range(1,N) if (E*D) % phi_N ==1]
 
-
+D = random.choice(private_key_candidates)
 print(f"D is: {D}")
 
 print(f"the public key is {E,N}")
-
 
 
 while True:
