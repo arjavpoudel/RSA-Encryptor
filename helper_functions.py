3438e13b3ba1bd10602dff3adb1661deb9de6c6f
arjavpoudel <123589124+arjavpoudel@users.noreply.github.com>
2023-02-23 16:44:49 +0000

    Update helper_functions.py

--- helper_functions.py
+++ helper_functions.py
@@ -1,6 +1,6 @@
 import random
 
-#checks whther a number in a given range is prime or not
+#checks whether a number in a given range is prime or not
 is_prime = lambda n: n >= 2 and all(n % i != 0 for i in range(2, int(n**0.5) + 1))
 
 #Generates a list of prime candidates between start and end.
