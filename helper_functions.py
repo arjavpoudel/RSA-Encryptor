c11f21f577b1dc2873d4b913592c78a2e8e7c742
arjavpoudel <123589124+arjavpoudel@users.noreply.github.com>
2023-02-23 16:27:58 +0000

    converted all to lambda functions
    converted all functions to lambda functions (that weren't already) with list comprehensions provided for brevity. Added comments for clarity.

--- helper_functions.py
+++ helper_functions.py
@@ -1,30 +1,17 @@
 import random
 
-
 #checks whther a number in a given range is prime or not
 is_prime = lambda n: n >= 2 and all(n % i != 0 for i in range(2, int(n**0.5) + 1))
-
 
 #Generates a list of prime candidates between start and end.
 get_prime_candidates = lambda start, end: [p for p in range(start, end) if is_prime(p)]
 
-def get_coprime(phi_N, N):
-    """
-    Generate a random coprime of phi_N and N.
-    """
-    while True:
-        e = random.randint(2, phi_N - 1)
-        if is_prime(e) and (e != N) and (phi_N % e != 0):
-            return e
+#generates a random comprime of phi_N and N.
+get_coprime = lambda phi_N, N: random.choice([e for e in range(2, phi_N - 1)  if is_prime(e) and (e != N) and (phi_N % e != 0)])
 
-
+#checks whether the product of E and D modulo n equals one. Pulls a random value from the list that satisfies the condition and assigns it to the variable.
 get_private_key = lambda E, phi_N: random.choice([D for D in range(1, phi_N) if (E*D) % phi_N == 1])
 
-
-def wordToNum(string):
-    ords = [ord(q) for q in list(string)]
-    sum = 0 
-    for num in ords:
-        sum+=num
-    return sum
+#converts user input string into array of chars, then sums over their ordinal values plus a random integer between 1 and 999.
+wordToNum = lambda string: sum(ord(q) for q in string) + random.randint(1, 1000)
 
